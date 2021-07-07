b0db40663f8aaff6c156d02cff28c3d3ff2a5159
Kavin Yao <kavinyao@gmail.com>
2021-07-07 15:51:31 -0700

    Swap layers

--- dz60-gaming/keymap.c
+++ dz60-gaming/keymap.c
@@ -1,9 +1,9 @@
 #include QMK_KEYBOARD_H
 
 enum dz60_layers {
+    _GL,  // gaming layer (qwerty with a few keys blocked)
     _BL,  // base layer (qwerty)
     _WL,  // work/coding layer (with layer tap on space)
-    _GL,  // gaming layer (with a few keys blocked)
     _FL,  // function layer
     _CL,  // control layer
 };
@@ -27,26 +27,26 @@ enum dz60_layers {
 // because QMK always falls back to layer 0 if no keycode is found.
 // Reference: https://www.reddit.com/r/olkb/comments/h95jk8/help_how_does_df_works_in_qmk/fux4bu4
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT_60_ansi_split_bs_rshift(
+    [_GL] = LAYOUT_60_ansi_split_bs_rshift(
         KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_DEL,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MY_UP,    DF(_GL),
-        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MY_LEFT,  MY_DOWN,  MY_RGHT
+        KC_GRV,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MY_UP,    DF(_BL),
+        KC_LCTL,  XXXXXXX,  XXXXXXX,                                KC_SPC,                                 KC_RALT,  MY_LEFT,  MY_DOWN,  MY_RGHT
+    ),
+    [_BL] = LAYOUT_60_ansi_split_bs_rshift(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DF(_WL),
+        _______,  KC_LGUI,  KC_LALT,                                _______,                                _______,  _______,  _______,  _______
     ),
     [_WL] = LAYOUT_60_ansi_split_bs_rshift(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         MY_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DF(_BL),
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DF(_GL),
         _______,  _______,  _______,                                MY_SPC,                                 _______,  _______,  _______,  _______
-    ),
-    [_GL] = LAYOUT_60_ansi_split_bs_rshift(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DF(_WL),
-        _______,  XXXXXXX,  XXXXXXX,                                KC_SPC,                                 _______,  _______,  _______,  _______
     ),
     [_FL] = LAYOUT_60_ansi_split_bs_rshift(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
@@ -94,7 +94,7 @@ void set_gaming_lighting(void) {
 // Reference: https://www.reddit.com/r/olkb/comments/9ny0jp/qmk_rgb_settings_done_in_matrix_init_user_are/e7qf83n
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
-    set_default_lighting();
+    set_gaming_lighting();
 }
 
 // NOTE: Using default_layer_state_set_user() instead of layer_state_set_user()
